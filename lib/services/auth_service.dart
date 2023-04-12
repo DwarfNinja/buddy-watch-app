8acecce26664e36826acb21519bf790c84c84caf
DwarfNinja <cenduroyib@gmail.com>
2023-04-12 22:48:24 +0200

    Feat: add auth service
    Implement supabase authentication client

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService {
+  final supabase = Supabase.instance.client;
+
+  Future<AuthResponse> logIn(String email, String password) async {
+    return await supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> logOut() async {
+    return await supabase.auth.signOut();
+  }
+
+  Future<AuthResponse> signUp(String email, String password) async {
+    return await supabase.auth.signUp(
+      email: email,
+      password: password,
+    );
+  }
+
+  Stream<AuthState> onAuthStateChange() {
+    return supabase.auth.onAuthStateChange;
+  }
+
+  bool isLoggedIn() {
+    final session = AuthService().supabase.auth.currentSession;
+    return session != null ? true : false;
+  }
+}
