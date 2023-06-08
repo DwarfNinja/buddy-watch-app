d334086aa1c3ea6ae526d07d9609bbd1d36733e2
DwarfNinja <cenduroyib@gmail.com>
2023-06-08 23:04:07 +0200

    Feat: refactor settings page to new design style
    Improve code and widget usage

--- lib/settings.dart
+++ lib/settings.dart
@@ -5,64 +5,73 @@ import 'package:flutter/material.dart';
 import 'login.dart';
 
 class Settings extends StatefulWidget {
-
-  const Settings({Key? key})
-      : super(key: key);
+  const Settings({Key? key}) : super(key: key);
 
   @override
   State<Settings> createState() => _SettingsState();
 }
-AuthService authService = AuthService();
-class _SettingsState extends State<Settings> {
 
+AuthService authService = AuthService();
+
+class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.blueGrey.shade900,
-            appBar: AppBar(
-              title: const Text('BuddyWatch'),
-              centerTitle: true,
-              backgroundColor: Colors.black,
+    return Scaffold(
+      backgroundColor: ColorPalette.darkGrey,
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Spacer(),
+                Text(
+                  "Settings",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700, color: Colors.white),
+                ),
+                Spacer(),
+              ],
             ),
-            body: Column(children: [
-              Expanded(
-                  child: SingleChildScrollView(
-                      child: Column(children: [
-                        Padding(
-                            padding: EdgeInsets.all(25),
-                            //apply padding to all four sides
-                            child: Align(
-                              alignment: Alignment.center,
-                              child: InkWell(
-                                child: Container(
-                                  height: 125.0,
-                                  width: 125.0,
-                                  color: Colors.transparent,
-                                  child: Container(
-                                    decoration: const BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius:
-                                        BorderRadius.all(Radius.circular(20.0))),
-                                    child: const Icon(
-                                      Icons.logout,
-                                      color: Colors.black,
-                                      size: 75.0,
-                                    ),
-                                  ),
-                                ),
-                                onTap: () {
-                                  authService.logOut();
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => const Login()),
-                                  );
-                                },
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 50),
+                    child: GestureDetector(
+                      child: SizedBox(
+                          height: 125,
+                          width: 125,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(20.0),
                               ),
-                            )
-                        )
-                      ])))
-            ])));
+                            ),
+                            child: const Icon(
+                              Icons.logout_rounded,
+                              color: Colors.black,
+                              size: 90,
+                            ),
+                          )),
+                      onTap: () {
+                        authService.logOut();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Login(),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
